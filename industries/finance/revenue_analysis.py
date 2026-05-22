58556e78274b8169275c8ced130c351ede708d0d
Suyash Tripathi <suyash.tripathi27@bimtech.ac.in>
2026-05-22 16:17:40 +0530

    Import SemanticValidator from utils.validator

--- industries/finance/revenue_analysis.py
+++ industries/finance/revenue_analysis.py
@@ -1,6 +1,7 @@
 """Revenue KPIs: totals, growth and average ticket."""
 import pandas as pd
 from .reliability import evaluate_kpi_confidence
+from utils.validator import SemanticValidator
 
 
 def _first_column(df, candidates):
