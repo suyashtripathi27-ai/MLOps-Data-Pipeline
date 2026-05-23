f7d7ef7dd45688742c5e5b5b1f39df73c7c8785a
Suyash Tripathi <suyash.tripathi27@bimtech.ac.in>
2026-05-23 18:30:36 +0530

    Update deposit_analysis.py

--- industries/banking/deposit_analysis.py
+++ industries/banking/deposit_analysis.py
@@ -2,7 +2,7 @@
 Deposit-specific KPIs (savings, checking, term deposits).
 """
 import pandas as pd
-from .reliability import evaluate_kpi_confidence
+from utils.kpi_helpers import first_column, safe_kpi, confidence_for
 from utils.validator import SemanticValidator
 
 
